8b34e7809aa105b3f0e72aa28117c445e3559c6c
iromashko <iromashko@me.com>
2020-09-02 11:03:19 +0300

    star rating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { CardComponent } from './components/card/card.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
+import { StarRatingsComponent } from './components/star-ratings/star-ratings.component';
 
 @NgModule({
-  declarations: [AppComponent, ToolbarComponent, CardComponent, AccordionComponent, ProgressBarComponent],
+  declarations: [AppComponent, ToolbarComponent, CardComponent, AccordionComponent, ProgressBarComponent, StarRatingsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
