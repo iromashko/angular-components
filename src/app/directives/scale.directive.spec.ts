266da1474cd4ddf2c3bdc4b57214b05f0da2cfc5
iromashko <iromashko@me.com>
2020-09-11 01:59:40 +0300

    common scripts

--- src/app/directives/scale.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ScaleDirective } from './scale.directive';
-
-describe('ScaleDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ScaleDirective();
-    expect(directive).toBeTruthy();
-  });
-});
